e102b8ed5ff19fa78ac40b61d1b10d761bf988c8
Lukas Bertsch <lukas.bertsch@tu-dortmund.de>
2024-04-22 13:46:20 +0200

    Daten

--- SciFi/plot.py
+++ SciFi/plot.py
@@ -6,21 +6,23 @@ from uncertainties import ufloat
 import scipy.constants as const
 from uncertainties.unumpy import nominal_values as noms
 from uncertainties.unumpy import std_devs as stds
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+import pandas as pd
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+# reading data
+df_sim = pd.DataFrame()
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+names = ["y_exit", "z_exit", "x_start", "y_start", "z_start", "px_start", "py_start", "pz_start", "reflCoCl", "reflClCl", "wl", "gpsPosX", "length_core", "length_clad", "rayleighScatterings"]
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+for i in range(90, 100): # range(1200)
+    frame = pd.read_csv(f"content/data/Simulation/job_{i}.txt", sep="\s+", comment = "#", names = names)
+    df_sim = df_sim.append(frame)
+
+print(df_sim.head())
+
+# prepare data
+
+df_sim["r_exit"] = np.sqrt(df_sim["y_exit"]**2 + df_sim["z_exit"]**2)
+df_sim = df_sim.drop(df_sim[df_sim.r_exit > 0.125].index)
+
+plt.hist(df_sim["r_exit"], bins = 100)
+plt.savefig("build/plot.pdf")
